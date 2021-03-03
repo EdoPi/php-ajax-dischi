3c0972a3f485b8439d0b51fc3ffaa24bf4025247
Edoardo Pizzicannella <edoardo.pizzicannella@gmail.com>
2021-03-03 19:39:32 +0100

    select completa

--- app/server.php
+++ app/server.php
@@ -2,5 +2,26 @@
 
 require_once __DIR__ . '/../database/database.php';
 
-header('Content-Type: application/json');
-echo json_encode($discs);
+
+if (array_key_exists ( 'genre' , $_GET ) && !empty($_GET['genre'])) {
+        $querySearch = $_GET['genre'];
+        foreach ($discs as $key => $value) {
+            if ($value['genre'] != $querySearch) {
+                unset($discs[$key]);
+            }
+        }
+    }
+
+ if (array_key_exists ( 'genre' , $_GET ) && empty($_GET['genre']) || count($discs) == 0) {
+     http_response_code(400);
+     exit();
+ }
+ header('Content-Type: application/json');
+ $jason = json_encode($discs);
+ echo $jason;
+
+
+
+
+/* header('Content-Type: application/json');
+echo json_encode($discs); */
--- js/script.js
+++ js/script.js
@@ -2,10 +2,41 @@ var app = new Vue({
     el: '#root',
     data: {
         discs: [],
+        arrayGenre: [],
+        selectGenre:'',
+        errorFlag: false,
         
     },
     methods: {
+
+        listForGenre: function() {
+            this.discs.forEach(element => {
+                if (!this.arrayGenre.includes(element.genre)) {
+                    this.arrayGenre.push(element.genre);
+                }
+            });
+        },
         
+        filterForGenre: function(){
+            const self = this;
+            if (self.selectGenre === 'all') {
+                axios.get('http://localhost:8888/php-ajax-dischi/app/server.php')
+                .then(function(resp) {
+                    console.log('discs ', resp.data);
+                    const disc = resp.data;
+                    self.discs = disc;               
+                });  
+            } else {
+                axios.get('http://localhost:8888/php-ajax-dischi/app/server.php?genre=' + self.selectGenre )
+                .then(function(resp) {
+                    console.log('discs ', resp.data);
+                    const disc = resp.data;
+                    self.discs = disc;               
+                }).catch(function(error){
+                    self.errorFlag = true;
+                });
+            }
+        },
 
     },
     mounted() {
@@ -14,7 +45,8 @@ var app = new Vue({
             .then(function(resp) {
                 console.log('discs ', resp.data);
                 const disc = resp.data;
-                self.discs = disc;                
+                self.discs = disc;
+                self.listForGenre();                
             });
 
 
